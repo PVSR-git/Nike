716a841d6b032cc2c6d9a69aeae23117f8465b52
PVSKR <skr.polu@hotmail.com>
2023-09-13 14:14:55 +0530

    this is my Nike app with the help of Tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hi,I am going to make Nike app...&#128512;
+      </header>
+    </div>
+  );
+}
+
+export default App;
