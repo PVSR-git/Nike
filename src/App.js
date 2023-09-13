f76f44e8be91587a28afcf14ebf8f78a47d22a1e
PVSKR <skr.polu@hotmail.com>
2023-09-13 22:29:51 +0530

    Deploying a few libraries and code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./index.css";
 function App() {
   return (
     <div>
-      <header className="text-3xl font-bold underline">
+      <header className="text-2xl text-blue-400 font-bold underline">
         Hi,I am going to make Nike app...&#128512;
       </header>
     </div>
