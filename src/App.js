819a784073ce97457649071359d66fbad110829d
PVSKR <skr.polu@hotmail.com>
2023-09-13 23:24:53 +0530

    created basic structure of nike site

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import "./index.css";
+import {
+  Hero,
+  SuperQuality,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+} from "./sections/index.js";
+// import Nav from "./sections/Nav";
 
-function App() {
-  return (
-    <div>
-      <header className="text-2xl text-blue-400 font-bold underline">
-        Hi,I am going to make Nike app...&#128512;
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <main className="relative">
+    {/* <Nav /> */}
+
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />{" "}
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8 text-white">
+      <Footer />
+    </section>
+  </main>
+);
 
 export default App;
---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,8 @@
+// rafce
+import React from "react";
+
+const CustomerReviews = () => {
+  return <div>CustomerReviews</div>;
+};
+
+export default CustomerReviews;
