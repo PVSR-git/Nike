d611d75ea6cfd110ff79627702d7800a68badb13
PVSKR <skr.polu@hotmail.com>
2023-09-16 01:51:30 +0530

    star and rating added to PopularProductCard.jsx

--- src/components/PapularProductCard.jsx
+++ src/components/PapularProductCard.jsx
@@ -5,8 +5,30 @@ const PapularProductCard = ({ imgURL, name, price }) => {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
       <img src={imgURL} alt={name} className="w-[128px} h-[280px]" />
-      <h3>{name}</h3>
-      <p>{price}</p>
+      <div
+        className="mt-8 flex
+      justify-start gap-2.5"
+      >
+        <img src={star} alt="rating" width={24} height={24} />
+        <p
+          className=" font-montserrat tex-xl 
+      leading-normal text-slate-gray"
+        >
+          (4.5)
+        </p>
+      </div>
+      <h3
+        className="mt-2 text-2xl leading-normal 
+      font-semibold font-palanquin"
+      >
+        {name}
+      </h3>
+      <p
+        className="mt-2 text-2lx font-montserrat
+       text-coral-red leading-normal "
+      >
+        {price}
+      </p>
     </div>
   );
 };
