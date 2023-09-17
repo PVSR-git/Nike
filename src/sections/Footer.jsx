1e119153023d613f2f362b30d30e085007fe0b97
PVSKR <skr.polu@hotmail.com>
2023-09-18 03:04:05 +0530

    CopyrightSign of footer.jsx styled with tailwind css

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -64,17 +64,31 @@ const Footer = () => {
           ))}
         </div>
       </div>
-      <div>
-        <div>
+      <div
+        className="flex justify-between
+      text-white-400 mt-24
+      max-sm:flex-col max-sm:items-center"
+      >
+        <div
+          className="flex flex-1
+        justify-start items-center gap-2
+        font-montserrat cursor-pointer"
+        >
           <img
             src={copyrightSign}
             alt="copy right sign"
             width={20}
             height={20}
+            className="rounded-full m-0"
           />
           <p>Copyright. All rights reserved.</p>
         </div>
-        <p>Terms & Conditions</p>
+        <p
+          className="font-montserrat
+        cursor-pointer"
+        >
+          Terms & Conditions
+        </p>
       </div>
     </footer>
   );
