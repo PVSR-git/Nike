818daa096d442bc31c54498f21253614f3d2d937
PVSKR <skr.polu@hotmail.com>
2023-09-18 02:01:12 +0530

    Subscribe.jsx section tailwind css updation  completed

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -16,9 +16,17 @@ const Subscribe = () => {
         Sign Up from
         <span className="text-coral-red"> Updates </span>& Newsletter
       </h3>
-      <div>
-        <input type="text" className="input" placeholder="subscribe@nike" />
-        <div>
+      <div
+        className="lg:max-w-[40%] w-full
+      flex items-center max-sm:flex-col
+      gap-5 p-2.5 sm:border sm:border-slate-gray
+      rounded-full"
+      >
+        <input type="text" className="input" placeholder="subscribe@nike.com" />
+        <div
+          className="flex max-sm:justify-end items-center
+        max-sm:w-full"
+        >
           <Button label="Sign Up" fullWidth />
         </div>
       </div>
